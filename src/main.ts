62291d7de9d28b10c9f1b25ede4a2639c9266e7b
Guidotss <guidoolguin@hotmail.com>
2023-05-05 15:57:58 -0300

    quitando global prefixer

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('Bootsrap');
 
-  app.setGlobalPrefix('/api/v1');
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
